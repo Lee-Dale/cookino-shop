e614990cc44764d639e6d29bedd130b1a49d28c3
Lee-Dale <247751855+Lee-Dale@users.noreply.github.com>
2026-05-07 15:06:39 +0200

    Updated Shop.py to include token verification

--- app/routes/shop.py
+++ app/routes/shop.py
@@ -3,12 +3,10 @@ from fastapi import APIRouter
 from Datenbanken_Cookinoshop.shop_main import alle_kollektionen, artikel_nach_kollektion, bestellung_aufgeben
 from app.models import Artikel, Kollektion, OrderRequest
 from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer
 from app.routes.auth import verify_token
 
 
 router = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 @router.get("/kollektionen")
 def get_kollektionen():
@@ -20,7 +18,6 @@ def get_kollektion_name(kollektion_name: str):
     return {"kollektion_name": [Artikel(**k) for k in artikel_nach_kollektion(kollektion_name)]}
 
 @router.post("/order")
-def place_order(order: OrderRequest, token: str = Depends(oauth2_scheme)):
-    verify_token(token)
+def place_order(order: OrderRequest, username: str = Depends(verify_token)):
     result = bestellung_aufgeben(order.artikel_id, order.menge)
-    return {"bestell_id": result}
+    return {"bestell_id": result, "ordered_by": username}
